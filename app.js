769453e80f1af55871f5242455606a2155fa7442
Ckopei <cameron@kopels.com>
2019-11-03 01:31:39 -0500

    redesign with bootstrap for responsiveness.

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-var express = require("express");
-var path = require('path')
-var PORT = 3000;
-var app = express();
+const express = require("express");
+const path = require('path')
+const PORT = 3000;
+const app = express();
+const router = express.Router();
 
 // public folder routes are not seen as router routes.
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/portfolio-controller");
-
-app.use(routes);
+router.get("/", function(req, res) {
+  // res.sendFile(path.join(__dirname+"/index.html"));
+  res.sendFile("index.html")
+});
 
 app.listen(PORT, function() {
   console.log("Server listening on: http://localhost:" + PORT);
--- public/assets/js/portfolio.js
+++ public/assets/js/portfolio.js
@@ -1,3 +1,11 @@
-$(document).ready(function(){
-    $('.parallax').parallax();
-  });
+var headerHeight = 200;
+
+$(window).bind('scroll', function () {
+if ($(window).scrollTop() > headerHeight) {
+    $('#myNav').removeClass('navbar-top');
+    $('#myNav').addClass('navbar-fixed-top');
+} else {
+    $('#myNav').removeClass('navbar-fixed-top');
+    $('#myNav').addClass('navbar-top');
+}
+}); 
